485e2289d380011ba5d1c09ff9fc42d345c4ff28
kevinacostaa <kevinmag999@gmail.com>
2025-01-25 20:33:41 -0300

    10-best_score.py

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary is None or a_dictionary == 0:
+    if a_dictionary is None or len(a_dictionary) == 0:
         return None
     key_max, maxim = list(a_dictionary.items())[0]
     for key, value in a_dictionary.items():
