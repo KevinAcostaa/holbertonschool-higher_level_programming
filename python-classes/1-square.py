c371cdbf19bc60dd9086e687358dec1c7e97e8b8
kevinacostaa <kevinmag999@gmail.com>
2025-01-28 12:59:01 -0300

    1-square.py

---
+++ python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+class Square
+"""
+
+
+class Square():
+    """
+    Square
+    """
+    def __init__(self, size):
+        """
+        Square
+        """
+        self.__size = size
