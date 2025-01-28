3c1c9295d6de4a91f3e87b5b6a4a09ff912a63df
kevinacostaa <kevinmag999@gmail.com>
2025-01-28 16:02:29 -0300

    5-square.py

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -22,10 +22,18 @@ class Square():
     @size.setter
     def size(self, value):
         if type(value) is not int:
-            raise TypeError("size must be an integer")
+            TypeError("size must be an integer")
         if value < 0:
-            raise ValueError("size must be >= 0")
+            ValueError("size must be >= 0")
         self.__size = value
 
     def area(self):
         return self.__size * self.__size
+    
+    def my_print(self):
+        if self.__size == 0:
+            print()
+        for i in range(self.__size):
+            for j in range(self.__size):
+                print("#", end="")
+            print()
