e48511480b635fbacc207094c7d73a198587cc7c
kevinacostaa <kevinmag999@gmail.com>
2025-01-31 14:09:17 -0300

    6-rectangle.py

--- python-more_classes/6-rectangle.py
+++ python-more_classes/6-rectangle.py
@@ -8,10 +8,12 @@ class Rectangle():
     """
     class Square
     """
+    num_of_instances = 0
+
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
-        Rectangle.number_of_instances += 1
+        Rectangle.num_of_instances += 1
 
     @property
     def width(self):
@@ -55,4 +57,4 @@ class Rectangle():
 
     def __del__(self):
         print("Bye rectangle...")
-        Rectangle.number_of_instances -= 1
+        Rectangle.num_of_instances -= 1
