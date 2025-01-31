126ab3de64a99c0b11005540a882d93e4f5ffc2c
kevinacostaa <kevinmag999@gmail.com>
2025-01-31 12:07:57 -0300

    1-safe_print_integer.py

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
     try:
-        "{:d}".format(value)
+        print("{:d}".format(value))
     except ValueError:
         return False
     except TypeError:
