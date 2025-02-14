5caa038fcaf218d68c55ad6e1bb45940edbea450
kevinacostaa <kevinmag999@gmail.com>
2025-02-14 17:04:55 -0300

    0-read_file.py

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python3
+"""
+read_file function is declared
+"""
+
 
 def read_file(filename=""):
+    """
+    function
+    """
     with open(filename, 'r', encoding='utf-8') as file:
-        print("Simple line", end="")
+        content = file.read()
+        print(content, end="")
