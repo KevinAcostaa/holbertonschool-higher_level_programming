b63b9199e69afb60cd7fb2844ba4e7e0e37ab94c
kevinacostaa <kevinmag999@gmail.com>
2025-02-14 17:45:16 -0300

    3-to_json_string.py

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+function is declared
+"""
+
+
+import json
+def to_json_string(my_obj):
+    """
+    function
+    """
+    my_obj_str = json.dumps(my_obj)
+    return my_obj_str
