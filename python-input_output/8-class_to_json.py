d8830c76ffc3f6dba41c94aac11a10f716f3c960
kevinacostaa <kevinmag999@gmail.com>
2025-02-16 19:39:59 -0300

    8-class_to_json.py

---
+++ python-input_output/8-class_to_json.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+function is declared
+"""
+import json
+
+
+def class_to_json(obj):
+    """
+    class to json
+    """
+    content = obj.__dict__
+    dic = {
+        clave: valor
+        for clave, valor in content.items()
+        if isinstance(valor, (list, dict, str, int, bool))
+    }
+    return dic
