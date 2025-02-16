e250660748cb92ea2a00351ab73ca9f020b23a3d
kevinacostaa <kevinmag999@gmail.com>
2025-02-16 19:26:56 -0300

    5-save_to_json_file.py

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -9,6 +9,9 @@ def save_to_json_file(my_obj, filename):
     """
     object a text
     """
+    if isinstance(my_obj, set):
+        my_obj_str = list(my_obj)
+    
     my_obj_str = json.dumps(my_obj)
     with open(filename, 'w') as file:
         try:
