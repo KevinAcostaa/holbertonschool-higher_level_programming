9fcb4731e6a195712ec4cb1cf8c83666b773de31
kevinacostaa <kevinmag999@gmail.com>
2025-02-21 18:33:20 -0300

    task_02_requests.py

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -4,7 +4,7 @@ import csv
 
 def fetch_and_print_posts():
     resp = requests.get('https://jsonplaceholder.typicode.com/posts')
-    print(f"Status code: {resp.status_code}")
+    print(f"Status Code: {resp.status_code}")
     
     if resp.status_code == 200:
         posts = resp.json()
@@ -30,9 +30,9 @@ def fetch_and_save_posts():
             'title': post['title'],
             'body': post['body']
         }
-    estruct_list.append(new_estruct)
+        estruct_list.append(new_estruct)
 
-    with open('posts.cvs', 'w') as file:
+    with open('posts.csv', 'w') as file:
         writer = csv.DictWriter(file, fieldnames=['id', 'title', 'body'])
         writer.writeheader()
         writer.writerows(estruct_list)
