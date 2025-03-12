aa408b35d792d04a47625bd576241754b65bba72
kevinacostaa <kevinmag999@gmail.com>
2025-03-12 16:15:26 -0300

    6-states.sql

---
+++ SQL_more_queries/6-states.sql
@@ -0,0 +1,6 @@
+-- Crear database hbtn_0d_usa y tabla states
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(256) NOT NULL
+)
