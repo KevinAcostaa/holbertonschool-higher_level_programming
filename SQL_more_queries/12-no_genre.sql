d9e500aec4487b76866af5f8bb4cd671ee88fb6f
kevinacostaa <kevinmag999@gmail.com>
2025-03-14 17:45:57 -0300

    12-no_genre.sql

--- SQL_more_queries/12-no_genre.sql
+++ SQL_more_queries/12-no_genre.sql
@@ -1,5 +1,6 @@
 --
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
-RIGHT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
+LEFT JOIN tv_show_genres ON tv_show.id = tv_show_genres.show_id
+WHERE tv_show_genres.genre_id IS NULL
+ORDER BY tv_show.title ASC, tv_show_genres.genre_id ASC;
