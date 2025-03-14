f99703439399ce539813be3ebcebbddc7011a082
kevinacostaa <kevinmag999@gmail.com>
2025-03-14 18:22:25 -0300

    13-count_shows_by_genre.sql

---
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,8 @@
+--
+SELECT tg.name AS genre,
+COUNT(ts.show_id) AS number_of_shows
+FROM tv_genres tg
+LEFT JOIN tv_show_genres ts ON tg.id = ts.genre_id
+GROUP BY tg.name
+HAVING number_of_shows > 0
+ORDER BY number_of_shows DESC;
