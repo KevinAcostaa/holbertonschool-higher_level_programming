3f1417ab24bf3fd3e5021823bee7db5b69d9bb99
kevinacostaa <kevinmag999@gmail.com>
2025-03-14 18:38:27 -0300

    14-my_genres.sql

--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -1,6 +1,6 @@
 --
 SELECT tg.name AS name
-FROM tv_genres
+FROM tv_genres tg
 JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
 JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id
 WHERE tv_show_genres.title = 'Dexter'
