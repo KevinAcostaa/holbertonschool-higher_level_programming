17e903e3518c6ec25641ec69b0b7b30ad495a161
kevinacostaa <kevinmag999@gmail.com>
2025-04-06 18:45:19 -0300

    0-add_integer.py

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+A function that adds 2 integers.
+"""
+
+
+def add_integer(a, b=98):
+    """
+    Adds two integers.
+    """
+    if not isinstance(alear, (int, float)):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
